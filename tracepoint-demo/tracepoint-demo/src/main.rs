529c6d6dcd9acfc647060f3fb168c735518e22f1
Vishal Patil <vishpat@gmail.com>
2022-09-21 13:14:07 +0000

    Added tracepoint demo project

---
+++ tracepoint-demo/tracepoint-demo/src/main.rs
@@ -0,0 +1,44 @@
+use aya::{include_bytes_aligned, Bpf};
+use aya::programs::TracePoint;
+use aya_log::BpfLogger;
+use clap::Parser;
+use log::{info, warn};
+use tokio::signal;
+
+#[derive(Debug, Parser)]
+struct Opt {
+    
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let opt = Opt::parse();
+
+    env_logger::init();
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    #[cfg(debug_assertions)]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/debug/tracepoint-demo"
+    ))?;
+    #[cfg(not(debug_assertions))]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/release/tracepoint-demo"
+    ))?;
+    if let Err(e) = BpfLogger::init(&mut bpf) {
+        // This can happen if you remove all log statements from your eBPF program.
+        warn!("failed to initialize eBPF logger: {}", e);
+    }
+    let program: &mut TracePoint = bpf.program_mut("tracepoint_demo").unwrap().try_into()?;
+    program.load()?;
+    program.attach("syscalls", "sys_enter_execve")?;
+
+    info!("Waiting for Ctrl-C...");
+    signal::ctrl_c().await?;
+    info!("Exiting...");
+
+    Ok(())
+}
