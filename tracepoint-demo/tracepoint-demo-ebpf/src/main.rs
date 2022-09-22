37f4c469b41cec1f6036c154b1dd55af7161c986
Vishal Patil <vishpat@gmail.com>
2022-09-22 02:49:49 +0000

    Able to print the filename

--- tracepoint-demo/tracepoint-demo-ebpf/src/main.rs
+++ tracepoint-demo/tracepoint-demo-ebpf/src/main.rs
@@ -4,23 +4,13 @@
 use aya_bpf::{
     cty::c_long,
     helpers::bpf_probe_read_user_str_bytes,
-    macros::{map, tracepoint},
+    macros::{tracepoint},
     maps::PerCpuArray,
     programs::TracePointContext,
 };
 use aya_log_ebpf::info;
 
-const LOG_BUF_CAPACITY: usize = 1024;
-
-#[repr(C)]
-pub struct Buf {
-    pub buf: [u8; LOG_BUF_CAPACITY],
-}
-
-#[map]
-pub static mut BUF: PerCpuArray<Buf> = PerCpuArray::with_max_entries(1, 0);
-
-#[tracepoint(name="tracepoint_demo")]
+#[tracepoint(name = "tracepoint_demo")]
 pub fn tracepoint_demo(ctx: TracePointContext) -> c_long {
     match try_tracepoint_demo(ctx) {
         Ok(ret) => ret,
@@ -31,26 +21,21 @@ pub fn tracepoint_demo(ctx: TracePointContext) -> c_long {
 fn try_tracepoint_demo(ctx: TracePointContext) -> Result<c_long, c_long> {
     const FILENAME_OFFSET: usize = 16;
     let filename_addr: u64 = unsafe { ctx.read_at(FILENAME_OFFSET)? };
-
-    // get the map-backed buffer that we're going to use as storage for the filename
-    let buf = unsafe {
-        let ptr = BUF.get_ptr_mut(0).ok_or(0)?;
-        &mut *ptr
-    };
+    let mut buf = [0u8; 128];
 
     // read the filename
     let filename = unsafe {
         core::str::from_utf8_unchecked(bpf_probe_read_user_str_bytes(
             filename_addr as *const u8,
-            &mut buf.buf,
+            &mut buf,
         )?)
     };
 
     if filename.len() < 512 {
         // log the filename
-        info!(&ctx, "open {}", filename);
+        info!(&ctx, "{}", filename);
     }
-    
+
     Ok(0)
 }
 
