c519fad9b1aaf0a90378f31477448c98f407da99
Vishal Patil <vishpat@gmail.com>
2022-10-03 01:40:01 +0000

    Prototype to block a binary from running

---
+++ block-binary/block-binary-ebpf/src/main.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![no_main]
+#[allow(non_upper_case_globals)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+#[allow(dead_code)]
+mod vmlinux;
+
+use aya_bpf::{macros::lsm, programs::LsmContext};
+use aya_log_ebpf::info;
+use vmlinux::task_struct;
+
+#[lsm(name = "task_alloc")]
+pub fn task_alloc(ctx: LsmContext) -> i32 {
+    match unsafe { try_task_alloc(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_task_alloc(ctx: LsmContext) -> Result<i32, i32> {
+    let task: *const task_struct = ctx.arg(0);
+    let name = &(*task).comm;
+    const BUF_SIZE: usize = 16;
+    let mut buffer: [u8; BUF_SIZE] = [0u8; BUF_SIZE];
+
+    let mut i:usize = 0;
+    for c in name.iter() {
+        buffer[i] = *c as u8;
+        i += 1;
+    }
+
+    info!(&ctx, "lsm hook task_alloc called {}", core::str::from_utf8_unchecked(&buffer));
+
+    Ok(0)
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
---
+++ block-binary/block-binary/src/main.rs
@@ -0,0 +1,52 @@
+use aya::{include_bytes_aligned, Bpf};
+use aya::{programs::Lsm, Btf};
+use aya_log::BpfLogger;
+use clap::Parser;
+use log::{info, warn};
+use simplelog::{ColorChoice, ConfigBuilder, LevelFilter, TermLogger, TerminalMode};
+use tokio::signal;
+
+#[derive(Debug, Parser)]
+struct Opt {}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let opt = Opt::parse();
+    TermLogger::init(
+        LevelFilter::Debug,
+        ConfigBuilder::new()
+            .set_target_level(LevelFilter::Error)
+            .set_location_level(LevelFilter::Error)
+            .build(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )?;
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    #[cfg(debug_assertions)]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/debug/block-binary"
+    ))?;
+    #[cfg(not(debug_assertions))]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/release/block-binary"
+    ))?;
+    if let Err(e) = BpfLogger::init(&mut bpf) {
+        // This can happen if you remove all log statements from your eBPF program.
+        warn!("failed to initialize eBPF logger: {}", e);
+    }
+    let btf = Btf::from_sys_fs()?;
+    let program: &mut Lsm = bpf.program_mut("task_alloc").unwrap().try_into()?;
+    info!("Loading task_alloc program");
+    program.load("task_alloc", &btf)?;
+    program.attach()?;
+
+    info!("Waiting for Ctrl-C...");
+    signal::ctrl_c().await?;
+    info!("Exiting...");
+
+    Ok(())
+}
