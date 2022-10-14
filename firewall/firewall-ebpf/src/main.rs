4db4ebb97b835da7b23fc1219af74ccf8efe60fe
Vishal Patil <vishpat@gmail.com>
2022-10-14 18:56:13 +0000

    Inital checkin

---
+++ firewall/firewall-ebpf/src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+use aya_bpf::{
+    bindings::xdp_action,
+    macros::xdp,
+    programs::XdpContext,
+};
+use aya_log_ebpf::info;
+
+#[xdp(name="firewall")]
+pub fn firewall(ctx: XdpContext) -> u32 {
+    match try_firewall(ctx) {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_ABORTED,
+    }
+}
+
+fn try_firewall(ctx: XdpContext) -> Result<u32, u32> {
+    info!(&ctx, "received a packet");
+    Ok(xdp_action::XDP_PASS)
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
